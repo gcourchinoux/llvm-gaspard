da1d0b3be88635c9830fa04adeadecb638e10f8f
gaspard C <gaspard@courchinoux.org>
2022-06-09 22:43:52 +0200

    priorité maintenant la génération du code

--- main.s
+++ main.s
@@ -8,15 +8,15 @@ get_next_virtual_adress:                ! @get_next_virtual_adress
 	.cfi_startproc
 ! %bb.0:
 	st	 fp, [-- GP]
-	add	 GP, 0x8,  fp
-	sub	 GP, 0x18,  GP
+	add	 GP, 0x8
+	sub	 GP, 0x18
 	ld	0[ fp],  GPR3
 	mov	hi(actuel_adress),  GPR3
-	or	 GPR3, lo(actuel_adress),  GPR3
+	or	 GPR3, lo(actuel_adress)
 	ld	0[ GPR3],  GPR9
 	ld	4[ GPR3],  GPR3
-	sub	 fp, 0x10,  GPR12
-	or	 GPR12, 0x4,  GPR12
+	sub	 fp, 0x10
+	or	 GPR12, 0x4
 	st	 GPR3, 0[ GPR12]
 	st	 GPR9, -16[ fp]
 	bt	.LBB0_1
@@ -35,10 +35,10 @@ get_next_virtual_adress:                ! @get_next_virtual_adress
 	nop
 .LBB0_2:                                !   in Loop: Header=BB0_1 Depth=1
 	mov	hi(actuel_adress),  GPR3
-	or	 GPR3, lo(actuel_adress),  GPR3
+	or	 GPR3, lo(actuel_adress)
 	ld	0[ GPR3],  GPR9
 	ld	4[ GPR3],  GPR12
-	add	 GPR12, 0x1,  GPR13
+	add	 GPR12, 0x1
 	sub.f	 GPR13,  GPR12, r0
 	sult	 GPR12
 	add	 GPR9,  GPR12
@@ -47,16 +47,16 @@ get_next_virtual_adress:                ! @get_next_virtual_adress
 	st	 GPR9, 0[ GPR3]
 .LBB0_3:                                !   in Loop: Header=BB0_1 Depth=1
 	ld	-20[ fp],  GPR3
-	add	 GPR3, 0x1,  GPR3
+	add	 GPR3, 0x1
 	bt	.LBB0_1
 	st	 GPR3, -20[ fp]
 .LBB0_4:
-	sub	 fp, 0x10,  GPR3
-	or	 GPR3, 0x4,  GPR3
+	sub	 fp, 0x10
+	or	 GPR3, 0x4
 	ld	0[ GPR3],  GPR9
 	ld	-16[ fp],  rv
 	ld	-4[fp], pc ! return
-	add	 fp, 0x0,  GP
+	add	 fp, 0x0
 	ld	-8[ fp],  fp
 .Lfunc_end0:
 	.size	get_next_virtual_adress, .Lfunc_end0-get_next_virtual_adress
@@ -70,16 +70,16 @@ init_pagination:                        ! @init_pagination
 	.cfi_startproc
 ! %bb.0:
 	st	 fp, [-- GP]
-	add	 GP, 0x8,  fp
-	sub	 GP, 0x30,  GP
+	add	 GP, 0x8
+	sub	 GP, 0x30
 	mov	0x10,  GPR3
 	st	 GPR3, 0[ GP]
-	add	 SP, 0x10,  rca
+	add	 SP, 0x10
 	st	 rca, [-- GP]
 	bt	get_next_virtual_adress
 	nop
 	mov	hi(pag_config),  GPR3
-	or	 GPR3, lo(pag_config),  GPR12
+	or	 GPR3, lo(pag_config)
 	st	 GPR12, -40[ fp]
 	st	 GPR9, 0[ GPR12]
 	ld	0[ GPR12],  GPR3
@@ -92,12 +92,12 @@ init_pagination:                        ! @init_pagination
 	st.b	 GPR9, 0[ GPR3]
 	mov	0xa0,  GPR3
 	st	 GPR3, 0[ GP]
-	add	 SP, 0x10,  rca
+	add	 SP, 0x10
 	st	 rca, [-- GP]
 	bt	get_next_virtual_adress
 	nop
 	mov	hi(pages),  GPR3
-	or	 GPR3, lo(pages),  GPR3
+	or	 GPR3, lo(pages)
 	st	 GPR9, 0[ GPR3]
 	ld	0[ GPR3],  GPR3
 	ld	-40[ fp],  GPR9
@@ -116,26 +116,26 @@ init_pagination:                        ! @init_pagination
 .LBB1_2:                                !   in Loop: Header=BB1_1 Depth=1
 	uld.b	-32[ fp],  GPR3
 	mov	0xe1,  GPR9
-	and	 GPR3,  GPR9,  GPR3
-	or	 GPR3, 0x6,  GPR3
+	and	 GPR3,  GPR9
+	or	 GPR3, 0x6
 	st.b	 GPR3, -32[ fp]
-	sub	 fp, 0x20,  GPR3
+	sub	 fp, 0x20
 	st	 GPR3, -36[ fp]
-	or	 GPR3, 0x1,  GPR3
+	or	 GPR3, 0x1
 	uld.b	0[ GPR3],  GPR9
 	mov	0xf0,  GPR12
-	and	 GPR9,  GPR12,  GPR9
+	and	 GPR9,  GPR12
 	st.b	 GPR9, 0[ GPR3]
 	mov	0x1,  GPR3
 	st	 GPR3, 0[ GP]
-	add	 SP, 0x10,  rca
+	add	 SP, 0x10
 	st	 rca, [-- GP]
 	bt	get_next_virtual_adress
 	nop
 	st	 GPR9, -20[ fp]
 	st	 rv, -24[ fp]
 	mov	hi(pages),  GPR3
-	or	 GPR3, lo(pages),  GPR3
+	or	 GPR3, lo(pages)
 	ld	0[ GPR3],  GPR9
 	ld	-12[ fp],  GPR12
 	sh	 GPR12, 0x3,  GPR12
@@ -145,7 +145,7 @@ init_pagination:                        ! @init_pagination
 	ld	-24[ fp],  GPR12
 	st	 GPR12, 16[ GPR9]
 	ld	-36[ fp],  GPR12
-	or	 GPR12, 0x4,  GPR12
+	or	 GPR12, 0x4
 	ld	0[ GPR12],  GPR12
 	st	 GPR12, 12[ GPR9]
 	ld	-32[ fp],  GPR12
@@ -155,43 +155,43 @@ init_pagination:                        ! @init_pagination
 	sh	 GPR12, 0x3,  GPR12
 	uld.h	[ GPR9 add  GPR12],  GPR13
 	mov	0xfff0,  GPR14
-	and	 GPR13,  GPR14,  GPR13
-	or	 GPR13, 0x3,  GPR13
+	and	 GPR13,  GPR14
+	or	 GPR13, 0x3
 	st.h	 GPR13, [ GPR9 add  GPR12]
 	ld	0[ GPR3],  GPR9
 	ld	-12[ fp],  GPR12
 	sh	 GPR12, 0x3,  GPR12
 	add	 GPR9,  GPR12
 	uld.h	0[ GPR13],  GPR13
-	or	 GPR13, 0x100,  GPR13
+	or	 GPR13, 0x100
 	st.h	 GPR13, [ GPR9 add  GPR12]
 	ld	0[ GPR3],  GPR3
 	ld	-12[ fp],  GPR9
 	sh	 GPR9, 0x3,  GPR9
 	uld.h	[ GPR3 add  GPR9],  GPR12
 	mov	0xff0f,  GPR13
-	and	 GPR12,  GPR13,  GPR12
+	and	 GPR12,  GPR13
 	bt	.LBB1_3
 	st.h	 GPR12, [ GPR3 add  GPR9]
 .LBB1_3:                                !   in Loop: Header=BB1_1 Depth=1
 	ld	-12[ fp],  GPR3
-	add	 GPR3, 0x1,  GPR3
+	add	 GPR3, 0x1
 	bt	.LBB1_1
 	st	 GPR3, -12[ fp]
 .LBB1_4:
 	mov	hi(pag_config),  GPR3
-	or	 GPR3, lo(pag_config),  GPR3
+	or	 GPR3, lo(pag_config)
 	ld	0[ GPR3],  GPR3
 	st	 GPR3, 4[ GP]
 	mov	hi(.L.str),  GPR3
-	or	 GPR3, lo(.L.str),  GPR3
+	or	 GPR3, lo(.L.str)
 	st	 GPR3, 0[ GP]
-	add	 SP, 0x10,  rca
+	add	 SP, 0x10
 	st	 rca, [-- GP]
 	bt	chiara_asm
 	nop
 	ld	-4[fp], pc ! return
-	add	 fp, 0x0,  GP
+	add	 fp, 0x0
 	ld	-8[ fp],  fp
 .Lfunc_end1:
 	.size	init_pagination, .Lfunc_end1-init_pagination
@@ -205,10 +205,10 @@ chiara_handle_bad_opcode:               ! @chiara_handle_bad_opcode
 	.cfi_startproc
 ! %bb.0:
 	st	 fp, [-- GP]
-	add	 GP, 0x8,  fp
-	sub	 GP, 0x8,  GP
+	add	 GP, 0x8
+	sub	 GP, 0x8
 	ld	-4[fp], pc ! return
-	add	 fp, 0x0,  GP
+	add	 fp, 0x0
 	ld	-8[ fp],  fp
 .Lfunc_end2:
 	.size	chiara_handle_bad_opcode, .Lfunc_end2-chiara_handle_bad_opcode
@@ -222,10 +222,10 @@ chiara_handle_no_stack_pointer:         ! @chiara_handle_no_stack_pointer
 	.cfi_startproc
 ! %bb.0:
 	st	 fp, [-- GP]
-	add	 GP, 0x8,  fp
-	sub	 GP, 0x8,  GP
+	add	 GP, 0x8
+	sub	 GP, 0x8
 	ld	-4[fp], pc ! return
-	add	 fp, 0x0,  GP
+	add	 fp, 0x0
 	ld	-8[ fp],  fp
 .Lfunc_end3:
 	.size	chiara_handle_no_stack_pointer, .Lfunc_end3-chiara_handle_no_stack_pointer
@@ -239,10 +239,10 @@ chiara_handle_bad_permissions:          ! @chiara_handle_bad_permissions
 	.cfi_startproc
 ! %bb.0:
 	st	 fp, [-- GP]
-	add	 GP, 0x8,  fp
-	sub	 GP, 0x8,  GP
+	add	 GP, 0x8
+	sub	 GP, 0x8
 	ld	-4[fp], pc ! return
-	add	 fp, 0x0,  GP
+	add	 fp, 0x0
 	ld	-8[ fp],  fp
 .Lfunc_end4:
 	.size	chiara_handle_bad_permissions, .Lfunc_end4-chiara_handle_bad_permissions
@@ -256,10 +256,10 @@ chiara_handle_bad_adress:               ! @chiara_handle_bad_adress
 	.cfi_startproc
 ! %bb.0:
 	st	 fp, [-- GP]
-	add	 GP, 0x8,  fp
-	sub	 GP, 0x8,  GP
+	add	 GP, 0x8
+	sub	 GP, 0x8
 	ld	-4[fp], pc ! return
-	add	 fp, 0x0,  GP
+	add	 fp, 0x0
 	ld	-8[ fp],  fp
 .Lfunc_end5:
 	.size	chiara_handle_bad_adress, .Lfunc_end5-chiara_handle_bad_adress
@@ -273,51 +273,51 @@ init_interrupts:                        ! @init_interrupts
 	.cfi_startproc
 ! %bb.0:
 	st	 fp, [-- GP]
-	add	 GP, 0x8,  fp
-	sub	 GP, 0x18,  GP
+	add	 GP, 0x8
+	sub	 GP, 0x18
 	mov	0xff0,  GPR3
 	st	 GPR3, 0[ GP]
-	add	 SP, 0x10,  rca
+	add	 SP, 0x10
 	st	 rca, [-- GP]
 	bt	get_next_virtual_adress
 	nop
 	mov	hi(chiara64_interrupt),  GPR3
-	or	 GPR3, lo(chiara64_interrupt),  GPR3
+	or	 GPR3, lo(chiara64_interrupt)
 	st	 GPR9, 0[ GPR3]
 	ld	0[ GPR3],  GPR9
 	uld.b	0[ GPR9],  GPR12
-	or	 GPR12, 0x1,  GPR12
+	or	 GPR12, 0x1
 	st.b	 GPR12, 0[ GPR9]
 	ld	0[ GPR3],  GPR9
 	mov	hi(chiara_handle_no_stack_pointer),  GPR12
-	or	 GPR12, lo(chiara_handle_no_stack_pointer),  GPR12
+	or	 GPR12, lo(chiara_handle_no_stack_pointer)
 	st	 GPR12, 12[ GPR9]
 	st	 GPR0, 8[ GPR9]
 	ld	0[ GPR3],  GPR9
 	uld.b	16[ GPR9],  GPR12
-	or	 GPR12, 0x1,  GPR12
+	or	 GPR12, 0x1
 	st.b	 GPR12, 16[ GPR9]
 	ld	0[ GPR3],  GPR9
 	mov	hi(chiara_handle_bad_permissions),  GPR12
-	or	 GPR12, lo(chiara_handle_bad_permissions),  GPR12
+	or	 GPR12, lo(chiara_handle_bad_permissions)
 	st	 GPR12, 28[ GPR9]
 	st	 GPR0, 24[ GPR9]
 	ld	0[ GPR3],  GPR9
 	uld.b	32[ GPR9],  GPR12
-	or	 GPR12, 0x1,  GPR12
+	or	 GPR12, 0x1
 	st.b	 GPR12, 32[ GPR9]
 	ld	0[ GPR3],  GPR9
 	mov	hi(chiara_handle_bad_opcode),  GPR12
-	or	 GPR12, lo(chiara_handle_bad_opcode),  GPR12
+	or	 GPR12, lo(chiara_handle_bad_opcode)
 	st	 GPR12, 44[ GPR9]
 	st	 GPR0, 40[ GPR9]
 	ld	0[ GPR3],  GPR9
 	uld.b	48[ GPR9],  GPR12
-	or	 GPR12, 0x1,  GPR12
+	or	 GPR12, 0x1
 	st.b	 GPR12, 48[ GPR9]
 	ld	0[ GPR3],  GPR3
 	mov	hi(chiara_handle_bad_adress),  GPR9
-	or	 GPR9, lo(chiara_handle_bad_adress),  GPR9
+	or	 GPR9, lo(chiara_handle_bad_adress)
 	st	 GPR9, 60[ GPR3]
 	st	 GPR0, 56[ GPR3]
 	mov	0x4,  GPR3
@@ -332,13 +332,13 @@ init_interrupts:                        ! @init_interrupts
 	nop
 .LBB6_2:                                !   in Loop: Header=BB6_1 Depth=1
 	mov	hi(chiara64_interrupt),  GPR3
-	or	 GPR3, lo(chiara64_interrupt),  GPR3
+	or	 GPR3, lo(chiara64_interrupt)
 	ld	0[ GPR3],  GPR9
 	ld	-12[ fp],  GPR12
 	sh	 GPR12, 0x4,  GPR12
 	uld.b	[ GPR9 add  GPR12],  GPR13
 	mov	0xfe,  GPR14
-	and	 GPR13,  GPR14,  GPR13
+	and	 GPR13,  GPR14
 	st.b	 GPR13, [ GPR9 add  GPR12]
 	ld	0[ GPR3],  GPR3
 	ld	-12[ fp],  GPR9
@@ -349,23 +349,23 @@ init_interrupts:                        ! @init_interrupts
 	st	 GPR0, 8[ GPR3]
 .LBB6_3:                                !   in Loop: Header=BB6_1 Depth=1
 	ld	-12[ fp],  GPR3
-	add	 GPR3, 0x1,  GPR3
+	add	 GPR3, 0x1
 	bt	.LBB6_1
 	st	 GPR3, -12[ fp]
 .LBB6_4:
 	mov	hi(chiara64_interrupt),  GPR3
-	or	 GPR3, lo(chiara64_interrupt),  GPR3
+	or	 GPR3, lo(chiara64_interrupt)
 	ld	0[ GPR3],  GPR3
 	st	 GPR3, 4[ GP]
 	mov	hi(.L.str.1),  GPR3
-	or	 GPR3, lo(.L.str.1),  GPR3
+	or	 GPR3, lo(.L.str.1)
 	st	 GPR3, 0[ GP]
-	add	 SP, 0x10,  rca
+	add	 SP, 0x10
 	st	 rca, [-- GP]
 	bt	chiara_asm
 	nop
 	ld	-4[fp], pc ! return
-	add	 fp, 0x0,  GP
+	add	 fp, 0x0
 	ld	-8[ fp],  fp
 .Lfunc_end6:
 	.size	init_interrupts, .Lfunc_end6-init_interrupts
@@ -379,17 +379,17 @@ init_restraint_mode:                    ! @init_restraint_mode
 	.cfi_startproc
 ! %bb.0:
 	st	 fp, [-- GP]
-	add	 GP, 0x8,  fp
-	sub	 GP, 0x10,  GP
+	add	 GP, 0x8
+	sub	 GP, 0x10
 	mov	hi(.L.str.2),  GPR3
-	or	 GPR3, lo(.L.str.2),  GPR3
+	or	 GPR3, lo(.L.str.2)
 	st	 GPR3, 0[ GP]
-	add	 SP, 0x10,  rca
+	add	 SP, 0x10
 	st	 rca, [-- GP]
 	bt	chiara_asm
 	nop
 	ld	-4[fp], pc ! return
-	add	 fp, 0x0,  GP
+	add	 fp, 0x0
 	ld	-8[ fp],  fp
 .Lfunc_end7:
 	.size	init_restraint_mode, .Lfunc_end7-init_restraint_mode
@@ -403,23 +403,23 @@ main:                                   ! @main
 	.cfi_startproc
 ! %bb.0:
 	st	 fp, [-- GP]
-	add	 GP, 0x8,  fp
-	sub	 GP, 0x8,  GP
-	add	 SP, 0x10,  rca
+	add	 GP, 0x8
+	sub	 GP, 0x8
+	add	 SP, 0x10
 	st	 rca, [-- GP]
 	bt	init_pagination
 	nop
-	add	 SP, 0x10,  rca
+	add	 SP, 0x10
 	st	 rca, [-- GP]
 	bt	init_interrupts
 	nop
-	add	 SP, 0x10,  rca
+	add	 SP, 0x10
 	st	 rca, [-- GP]
 	bt	init_restraint_mode
 	nop
-	or	 GPR0, 0x0,  rv
+	or	 GPR0, 0x0
 	ld	-4[fp], pc ! return
-	add	 fp, 0x0,  GP
+	add	 fp, 0x0
 	ld	-8[ fp],  fp
 .Lfunc_end8:
 	.size	main, .Lfunc_end8-main
